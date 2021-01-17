d60cbddaeea88345f5a76f17dd18d39ea19d8026
Dima <t0perxxl@gmail.com>
2021-01-17 11:45:35 +0200

    Reduced the work to one listener

--- js/index.js
+++ js/index.js
@@ -8,11 +8,12 @@ const REQUEST_URL = "https://randomuser.me/api/1.3/";
 const ALL_USERS_CARDS_FILTERS = allFriends.changeList;
 const ALL_USERS_CARDS = allFriends.currentList;
 const USERS_LIST = document.getElementById("usersList");
+const FILTERS_MENU = document.getElementById("filters_menu");
 const ABC_BUTTON_FILTER = document.getElementById("Abc");
 const ZYX_BUTTON_FILTER = document.getElementById("Zyx");
 const MALE_BUTTON_FILTER = document.getElementById("male");
 const FEMALE_BUTTON_FILTER = document.getElementById("female");
-const JUNIOR_FILTER_BUTTON = document.getElementById("junior-ftr");
+const YOUNGER_FILTER_BUTTON = document.getElementById("younger-ftr");
 const SENIOR_FILTER_BUTTON = document.getElementById("senior-ftr");
 const RESET_BUTTON = document.getElementById("reset");
 const SEARCH_INPUT = document.querySelector(".searcher");
@@ -65,11 +66,33 @@ function createUsersCards(users) {
   USERS_LIST.appendChild(lists);
 }
 
-ABC_BUTTON_FILTER.addEventListener("click", () => {
-  sortByABC();
+FILTERS_MENU.addEventListener("click", function ({ target }) {
+  switch (target) {
+    case ABC_BUTTON_FILTER:
+      sortingByABC();
+      break;
+    case ZYX_BUTTON_FILTER:
+      sortingByZYX();
+      break;
+    case YOUNGER_FILTER_BUTTON:
+      sortAscending();
+      break;
+    case SENIOR_FILTER_BUTTON:
+      descendingSort();
+      break;
+    case MALE_BUTTON_FILTER:
+      sortingMaleOnly();
+      break;
+    case FEMALE_BUTTON_FILTER:
+      sortingFemaleOnly();
+      break;
+    case RESET_BUTTON:
+      resettingFilters();
+      break;
+  }
 });
 
-function sortByABC() {
+function sortingByABC() {
   const abcSearch = ALL_USERS_CARDS_FILTERS.sort((a, b) => {
     if (a.name.first > b.name.first) {
       return 1;
@@ -84,11 +107,7 @@ function sortByABC() {
   createUsersCards(abcSearch);
 }
 
-ZYX_BUTTON_FILTER.addEventListener("click", () => {
-  sortByZYX();
-});
-
-function sortByZYX() {
+function sortingByZYX() {
   const zyxSearch = ALL_USERS_CARDS_FILTERS.sort((a, b) => {
     if (a.name.first < b.name.first) {
       return 1;
@@ -102,44 +121,43 @@ function sortByZYX() {
   createUsersCards(zyxSearch);
 }
 
-JUNIOR_FILTER_BUTTON.addEventListener("click", () => {
-  const younger = ALL_USERS_CARDS_FILTERS.sort((a, b) => a.dob.age - b.dob.age);
-  searchByAge(younger);
-});
-
-SENIOR_FILTER_BUTTON.addEventListener("click", () => {
-  const older = ALL_USERS_CARDS_FILTERS.sort((a, b) => b.dob.age - a.dob.age);
-  searchByAge(older);
-});
-
-function searchByAge(users) {
+function sortAscending() {
+  const youngers = ALL_USERS_CARDS_FILTERS.sort(
+    (a, b) => a.dob.age - b.dob.age
+  );
   resetCards();
-  createUsersCards(users);
+  createUsersCards(youngers);
 }
 
-MALE_BUTTON_FILTER.addEventListener("click", (elem) => {
+function descendingSort() {
+  const seniors = ALL_USERS_CARDS_FILTERS.sort((a, b) => b.dob.age - a.dob.age);
+  resetCards();
+  createUsersCards(seniors);
+}
+
+function sortingMaleOnly() {
   const filterMale = ALL_USERS_CARDS_FILTERS.filter(
     (elem) => elem.gender === "male"
   );
   resetCards();
   createUsersCards(filterMale);
-});
+}
 
-FEMALE_BUTTON_FILTER.addEventListener("click", (elem) => {
+function sortingFemaleOnly() {
   const filterFemale = ALL_USERS_CARDS_FILTERS.filter(
     (elem) => elem.gender === "female"
   );
   resetCards();
   createUsersCards(filterFemale);
-});
+}
+
+function resettingFilters() {
+  resetCards();
+  createUsersCards(ALL_USERS_CARDS);
+}
 
 SEARCH_INPUT.addEventListener("keydown", () => {
   search(ALL_USERS_CARDS_FILTERS);
-});
-
-RESET_BUTTON.addEventListener("click", () => {
-  resetCards();
-  createUsersCards(ALL_USERS_CARDS);
 });
 
 function search(people) {
