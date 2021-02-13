2d115a0b6fb9d213cc9b9569cbb33b6a0b8e6317
DIMA <t0perxxl@gmail.com>
2021-02-13 13:55:52 +0200

    fixed filters functions and main eventlistener

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const ALL_USERS_CARDS_FILTERS = friendsList.changed;
 const ALL_USERS_CARDS = friendsList.current;
 const USERS_LIST = document.getElementById("usersList");
 const FILTERS_MENU = document.getElementById("filters_menu");
-const RESET_BUTTON = document.getElementById("reset");
+let WHAT_FILTER_CHOSEN = "";
 const SEARCH_INPUT = document.querySelector(".searcher");
 
 function initApp() {
@@ -53,47 +53,50 @@ function createUsersCards(users) {
      <span class="age">Age: ${el.dob.age}</span>
      <span class="location">Location: ${el.location.city}</span>`;
     card.insertAdjacentHTML("beforeEnd", template);
-    lists.appendChild(card);
+    lists.append(card);
   });
 
   USERS_LIST.appendChild(lists);
 }
 
 FILTERS_MENU.addEventListener("click", function ({ target }) {
-  switch (target.value) {
-    case "nameAscend":
-      offChecked();
-      sortByAscendName();
-      break;
-    case "nameDescend":
-      offChecked();
-      sortByDescendName();
-      break;
-    case "younger":
-      offChecked();
-      sortAscend();
-      break;
-    case "senior":
-      offChecked();
-      descendSort();
-      break;
-    case "male":
-      offChecked();
-      sortByMale();
-      break;
-    case "female":
-      offChecked();
-      sortByFemale();
-      break;
-    case "reset":
-      offChecked();
-      resetFilters();
-      break;
+  if (target.nodeName === "INPUT") {
+    WHAT_FILTER_CHOSEN = target.value;
+    switch (target.value) {
+      case "nameAscend":
+        sortByName();
+        offChecked();
+        break;
+      case "nameDescend":
+        sortByName();
+        offChecked();
+        break;
+      case "younger":
+        sortByAge();
+        offChecked();
+        break;
+      case "senior":
+        sortByAge();
+        offChecked();
+        break;
+      case "male":
+        sortByGender();
+        offChecked();
+        break;
+      case "female":
+        sortByGender();
+        offChecked();
+        break;
+    }
+  }
+  if (target.value === "reset") {
+    resetFilters();
   }
 });
 
-function sortByAscendName() {
-  const increaseSearch = ALL_USERS_CARDS_FILTERS.sort((a, b) => {
+function sortByName() {
+  const nameSort = ALL_USERS_CARDS_FILTERS.slice();
+  const runSorting = (a, b) => {
     if (a.name.first > b.name.first) {
       return 1;
     }
@@ -101,54 +104,41 @@ function sortByAscendName() {
       return -1;
     }
     return 0;
-  });
-
+  };
+  if (WHAT_FILTER_CHOSEN === "nameAscend") {
+    nameSort.sort(runSorting);
+  } else {
+    nameSort.sort((a, b) => runSorting(b, a));
+  }
   resetCards();
-  createUsersCards(increaseSearch);
+  createUsersCards(nameSort);
 }
 
-function sortByDescendName() {
-  const decreaseSearch = ALL_USERS_CARDS_FILTERS.sort((a, b) => {
-    if (a.name.first < b.name.first) {
-      return 1;
-    }
-    if (a.name.first > b.name.first) {
-      return -1;
-    }
-    return 0;
-  });
+function sortByAge() {
+  const ageSort = ALL_USERS_CARDS_FILTERS.slice();
+  const runSorting = (a, b) => a.dob.age - b.dob.age;
+  if (WHAT_FILTER_CHOSEN === "younger") {
+    ageSort.sort(runSorting);
+  } else {
+    ageSort.sort((a, b) => runSorting(b, a));
+  }
   resetCards();
-  createUsersCards(decreaseSearch);
+  createUsersCards(ageSort);
 }
 
-function sortAscend() {
-  const youngers = ALL_USERS_CARDS_FILTERS.sort(
-    (a, b) => a.dob.age - b.dob.age
-  );
+function sortByGender() {
+  let genderFilter = [];
+  if (WHAT_FILTER_CHOSEN === "male") {
+    genderFilter = ALL_USERS_CARDS_FILTERS.filter(
+      (elem) => elem.gender === "male"
+    );
+  } else {
+    genderFilter = ALL_USERS_CARDS_FILTERS.filter(
+      (elem) => elem.gender === "female"
+    );
+  }
   resetCards();
-  createUsersCards(youngers);
-}
-
-function descendSort() {
-  const seniors = ALL_USERS_CARDS_FILTERS.sort((a, b) => b.dob.age - a.dob.age);
-  resetCards();
-  createUsersCards(seniors);
-}
-
-function sortByMale() {
-  const filterMale = ALL_USERS_CARDS_FILTERS.filter(
-    (elem) => elem.gender === "male"
-  );
-  resetCards();
-  createUsersCards(filterMale);
-}
-
-function sortByFemale() {
-  const filterFemale = ALL_USERS_CARDS_FILTERS.filter(
-    (elem) => elem.gender === "female"
-  );
-  resetCards();
-  createUsersCards(filterFemale);
+  createUsersCards(genderFilter);
 }
 
 function resetFilters() {
@@ -177,6 +167,7 @@ function resetCards() {
 }
 
 function offChecked() {
+  WHAT_FILTER_CHOISEN = "";
   let allInputs = document.querySelectorAll(".sort_input");
   allInputs.forEach((element) => {
     element.checked = false;
